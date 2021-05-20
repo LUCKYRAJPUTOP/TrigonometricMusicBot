03bade2d1706b0bf42dd0fbe93672ccf3d067083
Anshul Rajput <82425167+LUCKYRAJPUTOP@users.noreply.github.com>
2021-05-20 15:47:26 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,25 +9,25 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @AuraXSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @VibeXSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Support⚡️", url="https://t.me/AuraXSupport"
+                        "Support⚡️", url="https://t.me/VibeXSupport"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Creater⚡️", url="https://t.me/AuraX_Owner"
+                        "Creater⚡️", url="https://t.me/VibeXOwner"
                     ),
                     InlineKeyboardButton(
-                        "PglZone⚡️", url="https://t.me/PglZone"
+                        "VibeXZone⚡️", url="https://t.me/VibeXZone"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Add To Your Group⚡️", url="https://t.me/AuraXMusicBot?startgroup=true"
+                        "Add To Your Group⚡️", url="https://t.me/VibeXMusicBot?startgroup=true"
                     )
                 ]
             ]
